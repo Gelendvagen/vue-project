745fc50411c90962faefd9a2d834f8cbb7fdfa79
Angelina_Prokina <angelinaprokina@yandex.ru>
2024-11-06 21:07:03 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
-  base: '/vite-deploy-demo/',
   plugins: [
     vue(),
     vueJsx(),
